0b4d1b3ac715533584e4b2adf01d6e21b7608b64
Saarim Zaid <saarimzaid@gmail.com>
2023-03-17 18:07:29 +0530

    'edit-Food.js-file'

--- src/router/Food.js
+++ src/router/Food.js
@@ -1,7 +1,12 @@
+import { Fname } from "App.js"
+import { useContext } from "react"
 
 const Food = () => {
+    const fname = useContext(Fname)
     return <>
-
+        <h1>hello</h1>
+        {fname}
+        {/* <button>{fname} </button> */}
     </>
 }
 
